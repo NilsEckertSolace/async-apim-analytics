370e5c433f401e5ce01c6ddcf67117fcbd33ac25
Nils Eckert <nils.eckert@solace.com>
2022-04-26 12:36:49 +0200

    fixes

--- apim-analytics-server/test/integration/setup.ts
+++ apim-analytics-server/test/integration/setup.ts
@@ -11,24 +11,33 @@ const connectorServerDirectory = `${scriptDir}/../resources/apim-connector`;
 
 export async function mochaGlobalSetup() {
 
+  logger.log(scriptName, 'Setup test environment ...');
+
   // create and start local connector
   const code = s.exec(`bash ${connectorServerDirectory}/setup.sh 2>&1`, { silent: false }).code
   if (code != 0) {
-    throw new Error('Setup of API Management Connector failed');
+    logger.log(scriptName, 'Setup of API Management Connector failed');
+    process.exit(1);
   }
 
   // start analytics server
-  await request(server).get('/').expect(200);
+  await request(server).get('/').expect(200).catch((error) => {
+    logger.log(scriptName, 'Start of API Management Analytics Server failed');
+    process.exit(1);
+  });
 
   logger.log(scriptName, 'Setup finished');
 }
 
 export async function mochaGlobalTeardown() {
 
+  logger.log(scriptName, 'Teardown test environment ...');
+
   // stop and remove local connector 
   const code = s.exec(`bash ${connectorServerDirectory}/teardown.sh 2>&1`, { silent: false }).code
   if (code != 0) {
-    throw new Error('Teardown of API Management Connector failed');
+    logger.log(scriptName, 'Teardown of API Management Connector failed');
+    process.exit(1);
   }
 
   logger.log(scriptName, 'Teardown finished');
